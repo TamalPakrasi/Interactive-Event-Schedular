f83835d3a34c029264fc0bbfea8dd808fd09e1ca
Tamal Pakrasi <tamalpakrasi11@gmail.com>
2025-05-14 23:39:31 +0530

    add datepicker

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,3 +1,29 @@
 $(document).ready(function () {
   console.log('script is running....');
+
+  function datepickerWidthAdjust(input) {
+    const inputWidth = $(input).outerWidth();
+    setTimeout(() => {
+      $("#ui-datepicker-div").css({
+        width: inputWidth + "px"
+      });
+    }, 0);
+  }
+
+  $("#inputDate").datepicker({
+    showButtonPanel: true,
+    dateFormat: "dd-mm-yy",
+    beforeShow: function (input) {
+      datepickerWidthAdjust(input);
+    },
+    onChangeMonthYear: function () {
+      datepickerWidthAdjust($("#inputDate"));
+    }
+  });
+
+  $('#inputTime').timepicker();
+
+  $('[data-bs-dismiss="modal"]').click(function () {
+    $('#eventModal input').val("");
+  });
 });
